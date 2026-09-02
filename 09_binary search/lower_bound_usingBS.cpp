bcbb54f2de652b44a91d3b6e27a3fa0de57986f2
Manthan Borse <manthanborse733@gmail.com>
2026-09-02 20:36:51 +0530

    problem on Binary Search

---
+++ 09_binary search/lower_bound_usingBS.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+int lower_bound(std::vector<int>arr,int n,int target){
+    int st=0;
+    int end=n-1;
+    int ans=-1;
+    while(st<=end){
+        int mid=st+(end-st)/2;
+        if(arr[mid]>=target){
+            ans=mid;
+            end=mid-1;
+        }
+        else{
+            st=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n,target;
+    std::cout<<"enter the size of array:";
+    std::cin>>n;    
+    std::vector<int> arr(n);
+    for(int i=0;i<n;i++){
+            std::cout<<"enter the "<<i+1<<"th element of array :";
+            std::cin>>arr[i];
+        }
+    std::cout<<"enter the target element to be searched :";
+    std::cin>>target;
+
+    std::cout<<"the lower bound of the target element is at index :"<<lower_bound(arr,n,target);
+}
